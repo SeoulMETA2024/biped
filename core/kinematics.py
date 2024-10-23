3d84258a95774265b410a8c2035e3c06f516438a
ehdrjs13 <ehdrjs070102@gmail.com>
2024-10-23 15:19:57 +0900

    add controlling code structures

---
+++ core/kinematics.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Kinematics:
+    def __init__(self):
+        pass
+
+    def forwardKinematics(theta1,theta2, theta3):
+
+        return 
+    
+    def inverseKinematics(x,y):
+        return
