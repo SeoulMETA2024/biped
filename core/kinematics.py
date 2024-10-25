1031ce429602a2a636cb9c21418d74cc72a732bb
ehdrjs13 <ehdrjs070102@gmail.com>
2024-10-25 23:55:38 +0900

    implement kinematics lib

--- core/kinematics.py
+++ core/kinematics.py
@@ -1,12 +1,27 @@
 import numpy as np
 
+
 class Kinematics:
-    def __init__(self):
+    #https://ddangeun.tistory.com/27 2DOF manipulator kinematics 수식 참고
+    def __init__(self, l1, l2):
+        
+        self.L1 = l1
+        self.L2 = l2
         pass
 
-    def forwardKinematics(theta1,theta2, theta3):
+    def forwardKinematics(self,theta1,theta2):
+        
+        x = self.L1 * np.cos(theta1) + self.L2 * np.cos(theta1+theta2)
+        y = self.L1 * np.sin(theta1) + self.L2 * np.cos(theta1+theta2)
 
-        return 
+        return (x,y)
     
-    def inverseKinematics(x,y):
+    def inverseKinematics(self,x,y):
+
+
+        cos_theta2 = (x**2+y**2-self.L1**2-self.L2**2)/(2*self.L1*self.L2)
+
+        sin_theta2 = np.sqrt(1-cos_theta2**2)
+        theta2 = np.arctan2(sin_theta2,cos_theta2)
+        theta1 = np.arctan2(y,x) - np.arctan2(self.L1 + self.L2*cos_theta2,self.L2*sin_theta2) 
         return
